8d4f4cc887815aa80bbd6e70dac5ed0f5e84ccdb
allansmithll <allanammancio2004@gmail.com>
2023-09-01 09:46:51 -0300

    Quase finalizando

--- js/controlador/conta-controller.js
+++ js/controlador/conta-controller.js
@@ -19,16 +19,25 @@ class ContaController {
         const elementoSaldo = document.querySelector('#saldo');
         const elementoDataAniversario = document.querySelector('#data_aniversario');
         const elementoTipoConta = document.querySelector("#tipo-conta");
+        let conta = null;
         
         elementoTipoConta.addEventListener("change", () => {
             const selectedValue = elementoTipoConta.value;
-            console.log("Opção seleccionado: " + selectedValue);
-        });
-        
-        const conta = new Conta(elementoNumero.value,
-            Number(elementoSaldo.value));
-        this.repositorioContas.adicionar(conta);
-        this.inserirContaNoHTML(conta);
+
+            if (selectedValue == "conta-bonificada") {
+                conta = new ContaBonificada(elementoNumero.value, Number(elementoSaldo.value));
+            } else if (selectedValue == "conta-poupanca") {
+                conta = new Poupanca(elementoNumero.value, Number(elementoSaldo.value), Number(elementoDataAniversario.value));
+            } else if (selectedValue == "conta-normal") {
+                conta = new Conta(elementoNumero.value, Number(elementoSaldo));
+            } else {
+                    window.alert("Não foi possível criar a conta. Preencha corretamente os campos para o tipo da Conta.")
+                    return
+                }
+                this.adicionarConta(conta);
+                this.adicionarContaNoHtml(conta);
+            }  
+        );
     }
 
     inserirContaNoHTML(conta) {
