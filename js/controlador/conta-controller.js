44789b5a68b845d1b70223f186afbce0d46c49a7
allansmithll <allanammancio2004@gmail.com>
2023-09-01 17:45:25 -0300

    Terminando apenas a criação da criaçãoda Poupanca

--- js/controlador/conta-controller.js
+++ js/controlador/conta-controller.js
@@ -9,7 +9,7 @@ class ContaController {
 
     listar() {
         this.repositorioContas.getContas().forEach(conta =>
-            this.inserirContaNoHTML(conta)
+            this.inserirContaNoHtml(conta)
         );
     }
 
@@ -17,30 +17,31 @@ class ContaController {
         evento.preventDefault();
         const elementoNumero = document.querySelector('#numero');
         const elementoSaldo = document.querySelector('#saldo');
-        const elementoDataAniversario = document.querySelector('#data_aniversario');
         const elementoTipoConta = document.querySelector("#tipo-conta");
-        let conta = null;
-        
-        elementoTipoConta.addEventListener("change", () => {
-            const selectedValue = elementoTipoConta.value;
 
-            if (selectedValue == "conta-bonificada") {
-                conta = new ContaBonificada(elementoNumero.value, Number(elementoSaldo.value));
-            } else if (selectedValue == "conta-poupanca") {
-                conta = new Poupanca(elementoNumero.value, Number(elementoSaldo.value), Number(elementoDataAniversario.value));
-            } else if (selectedValue == "conta-normal") {
-                conta = new Conta(elementoNumero.value, Number(elementoSaldo));
-            } else {
-                    window.alert("Não foi possível criar a conta. Preencha corretamente os campos para o tipo da Conta.")
-                    return
-                }
-                this.adicionarConta(conta);
-                this.adicionarContaNoHtml(conta);
-            }  
-        );
-    }
+        const numeroConta = elementoNumero.value.trim();
+        const saldoConta = Number(elementoSaldo.value.trim());
 
-    inserirContaNoHTML(conta) {
+        if (elementoTipoConta.value == "conta-bonificada") {
+            const conta = new ContaBonificada(numeroConta, saldoConta);
+            this.adicionarConta(conta);
+            this.inserirContaNoHtml(conta);
+        } else if (elementoTipoConta.value == "conta-poupanca") {
+            const elementoDataAniversario = document.querySelector('#data_aniversario');
+            const conta = new Poupanca(numeroConta, saldoConta, elementoDataAniversario.value);
+            this.adicionarConta(conta);
+            this.inserirContaNoHtml(conta);
+        } else if (elementoTipoConta.value == "conta-normal") {
+            const conta = new Conta(numeroConta, Number(saldoConta));
+            this.adicionarConta(conta);
+            this.inserirContaNoHtml(conta);
+        } else {
+            window.alert("Não foi possível criar a conta. Preencha corretamente os campos para o tipo da Conta.");
+            return;
+        }
+    }  
+
+    inserirContaNoHtml(conta) {
         const elementoP = document.createElement('p');
         elementoP.textContent = 'Conta ' + conta.numero + ': ' + conta.saldo;
         const botaoApagar = document.createElement('button');
